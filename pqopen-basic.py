cf13546a3bf397172445a9d7718772c2b46af4b5
Michael Oberhofer <michael@oberhofer.me>
2024-12-05 17:01:37 +0100

    pqopen: use storagecontroller for writing csv's

--- pqopen-basic.py
+++ pqopen-basic.py
@@ -1,29 +1,22 @@
 import tomllib
 import time
-from pathlib import Path
 import zmq
+import uuid
 
 from daqopen.channelbuffer import AcqBufferPool
 from daqopen.daqzmq import DaqSubscriber
 from daqopen.helper import GracefulKiller
 from pqopen.powersystem import PowerSystem
-
-class CsvWriter(object):
-    def __init__(self, filename: str, keys: list):
-        self.header_keys = keys
-        self.file_path = Path(filename)
-        self.file_path.write_text("timestamp," + ",".join(keys)[:-2]+"\n")
-
-    def write_line(self, timestamp_us: int, data: dict):
-        with open(self.file_path, "a") as f:
-            f.write(f"{timestamp_us/1e6:.3f},")
-            f.write(",".join([f"{data[key]:.3f}" if isinstance(data[key], float) else "" for key in self.header_keys])+"\n")
+from pqopen.storagecontroller import StorageController, StoragePlan, CsvStorageEndpoint
 
 with open("config/pqopen-simple.toml", "rb") as f:
     config = tomllib.load(f)
 
 # Initialize App Killer
 app_terminator = GracefulKiller()
+
+# Generate measurement id
+measurement_id = str(uuid.uuid4())
 
 # Subscribe to DaqOpen Zmq Server
 daq_sub = DaqSubscriber(config["zmq_server"]["host"], config["zmq_server"]["port"])
@@ -45,12 +38,21 @@ for phase_name, phase in config["powersystem"]["phase"].items():
                            i_channel=daq_buffer.channel[phase["i_channel"]])
 power_system.enable_harmonic_calculation()
 power_system.enable_nper_abs_time_sync(daq_buffer.time, interval_sec=10)
+
+# Initialize Storage Controller
+storage_controller = StorageController(time_channel=daq_buffer.time, sample_rate=daq_sub.daq_info.board.samplerate)
+csv_storage_endpoint = CsvStorageEndpoint("csv", measurement_id, "data")
+csv_storage_plan = StoragePlan(storage_endpoint=csv_storage_endpoint,
+                               start_timestamp_us=int(daq_sub.timestamp*1e6),
+                               interval_seconds=1)
+for channel in power_system.output_channels.values():
+    if len(channel._data.shape) == 1:
+        csv_storage_plan.add_channel(channel)
+storage_controller.add_storage_plan(csv_storage_plan)
     
 # Initialize Acq variables
 print_values_timestamp = time.time()
 last_print_values_acq_sidx = 0
-output_channel_keys = list(power_system.output_channels.keys())
-csv_writer = CsvWriter("power_values.csv", output_channel_keys)
 
 # Initialize ZMQ Publisher
 context = zmq.Context()
@@ -62,10 +64,4 @@ while not app_terminator.kill_now:
     m_data = daq_sub.recv_data()
     daq_buffer.put_data_with_timestamp(m_data, int(daq_sub.timestamp*1e6))
     power_system.process()
-    if time.time() > print_values_timestamp + 1:
-        power_data = power_system.get_aggregated_data(last_print_values_acq_sidx, daq_buffer.actual_sidx)
-        socket.send_pyobj(power_data)
-        ts = daq_buffer._last_timestamp_us
-        csv_writer.write_line(ts, power_data)
-        last_print_values_acq_sidx = daq_buffer.actual_sidx
-        print_values_timestamp = time.time()
+    storage_controller.process()
